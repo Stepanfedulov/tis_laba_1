e8d28c1f1d34ad3458fa6ca452abecba9823b26e
Stepan Fedulov <safifed23532@gmail.com>
2024-09-11 15:48:14 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+
+  
+  const users = [
+      { name: "user1", surn: "surn1", age: 30 },
+      { name: "user2", surn: "surn2", age: 31 },
+      { name: "user3", surn: "surn3", age: 32 },
+    ];
+    
+    
+      
+  }
+  
+
+
+export default App;
