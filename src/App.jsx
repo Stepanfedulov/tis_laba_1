7753d0518f6a85fb26c9a81591a516d79c788cc0
Stepan Fedulov <safifed23532@gmail.com>
2024-09-11 15:59:51 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import "./App.css";
+const prods = [
+  { id: 1, name: "продукт1", cost: 100 },
+  { id: 2, name: "продукт2", cost: 200 },
+  { id: 3, name: "продукт3", cost: 300 }
+];
 
 function App() {
-
-  
-  const users = [
-      { name: "user1", surn: "surn1", age: 30 },
-      { name: "user2", surn: "surn2", age: 31 },
-      { name: "user3", surn: "surn3", age: 32 },
-    ];
-    
-    
-      
-  }
-  
-
+  const res = prods.map(item => (
+    <li key={item.id}>
+      {item.name}: {item.cost}
+    </li>
+  ));
+  return <ul>{res}</ul>;
+}
 
 export default App;
